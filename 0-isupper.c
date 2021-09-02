a18ce272bde44020d886b5afe700c978bfa19201
Eyoeal <novamax418@gmail.com>
2021-09-02 11:23:15 -0700

    first commit

---
+++ 0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks whether a char is uppercase or not 
+ * @c: the char to be checked
+ *
+ * Return: 1 if c is uppercase
+ * 0 otherwise 
+ */
+int _isupper(int c)
+{
+  if(c >= 75 && c <=90)
+    {
+      return (1);
+    }
+  else
+    {
+      return(0);
+    }
+}
